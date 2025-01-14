82040a36cf2c4c8f40b9473108b80f22f3c0d4dd
Hernandessn <hernandessalesneves7@gmail.com>
2025-01-13 22:17:43 -0300

    Adicionada biblioteca Stripe para meios de pagamento

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,15 @@
+require('dotenv').config(); // Importa o dotenv e carrega as variáveis do .env
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'devburger',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
-  };
-  
+  dialect: process.env.PG_DIALECT, // Lê o dialeto do .env
+  host: process.env.PG_HOST, // Corrige o prefixo para PG_
+  port: process.env.PG_PORT, // Corrige o prefixo para PG_
+  username: process.env.PG_USERNAME, // Corrige o prefixo para PG_
+  password: process.env.PG_PASSWORD, // Corrige o prefixo para PG_
+  database: process.env.PG_DATABASE, // Corrige o prefixo para PG_
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import SessionController from "./app/controllers/SessionController";
 import ProductController from "./app/controllers/ProductController";
 import CategoryController from "./app/controllers/CategoryController";
 import OrderController from "./app/controllers/OrderController";
-
+import CreatePaymentIntentController from "./app/controllers/stripe/CreatePaymentIntentController";
 import authMiddlware from "./app/middlewares/auth";
 
 
@@ -32,6 +32,7 @@ routes.post("/orders",OrderController.store);
 routes.get("/orders",OrderController.index);
 routes.put("/orders/:id",OrderController.update);
 
+routes.post("/create-payment-intent",CreatePaymentIntentController.store);
 
 
 export default routes;
